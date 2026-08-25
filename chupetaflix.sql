3472a69cf7439bea1df31a62d78694a6d456b4e8
Samuel Ogliari <samuel.ogliari@universo.univates.br>
2026-08-25 16:02:21 -0300

    Acrescenta Projeto Antigo, sem alterações

---
+++ chupetaflix.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  SAUDE
+ * Created: 25 de ago. de 2026
+ */
+
